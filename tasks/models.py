87f0166f9d2a93aeb72773c907dd4ffc1c88e032
Asel <aselalybaeva42@gmail.com>
2021-08-10 20:53:30 +0600

    Set up tasks

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Task(models.Model):
+    body = models.TextField()
+    estimated_finish_time = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+
+
