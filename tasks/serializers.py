0c8448e4db87c330a7610a85aa29ee475567b539
Asel <aselalybaeva42@gmail.com>
2021-08-13 00:22:20 +0600

    We added UserSerializer for creator

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email',]
+
 
 class TaskSerializer(serializers.Serializer):
-    body = serializers.CharField(required=False)
+    creator = UserSerializer(read_only=True)
+    body = serializers.CharField()
     estimated_finish_time = serializers.DateTimeField(format="%d-%m-%Y %H:%M", input_formats=['%d-%m-%Y %H:%M', ])
     is_completed = serializers.BooleanField(read_only=True)
     created_datetime = serializers.DateTimeField(read_only=True, format="%d-%m-%Y %H:%M")
