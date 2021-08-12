261ad4c11dc3c9a6c5760ea2d6af98d88f2e6aeb
Asel <aselalybaeva42@gmail.com>
2021-08-13 00:21:26 +0600

    We added new attribute creator for tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Task(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=True)
     body = models.TextField()
     estimated_finish_time = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
